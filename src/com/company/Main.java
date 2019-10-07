42b97bdfecb7fab27e03c617092ff29855e20cf6
Serov Maxim <maxim58r@gmail.com>
2019-10-07 18:28:52 +0300

    new

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,20 +1,11 @@
 package com.company;
 
-class Main {
+import com.company.View.GameWindow;
+
+public class Main {
     public static void main(String[] args) {
-        Field field = new Field();
+        GameWindow gameWindow = new GameWindow();
+        gameWindow.init();
 
-        field.init();
-        int j = 0;
-        for (int i = 4; i > 0; i--) {
-            j++;
-            field.setShip(i, j);
-        }
-        field.viewField();
-
-        do {
-            field.shoot();
-            field.viewField();
-        } while ((field.notEndGame()));
     }
 }
